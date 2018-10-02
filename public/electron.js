4a5dbb416623f5323134b51a69381fd3dc71de6d
Dion Pettaway <dionp@fullsail.edu>
2018-10-02 13:30:32 -0400

    Got electron + CRA working kinda

---
+++ public/electron.js
@@ -0,0 +1,31 @@
+const electron = require('electron');
+const path = require('path');
+const {app, Tray, ipcMain, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+const NOTES = []
+const WIDTH = 575;
+const HEIGHT = 650;
+const MAX_CHARACTERS = 45;
+
+function createWindow() {
+  
+  mainWindow = new BrowserWindow({
+    width: WIDTH,
+    height: HEIGHT,
+    show: false,
+    frame: false,
+    transparent: true
+  });
+
+  if (process.env.NODE_ENV === 'production') {
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, '../build/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+  } else {
+    mainWindow.loadURL('http://localhost:3000')
+  }
+}
