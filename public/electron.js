11ff8e42a5f1132545f4a3cfd2a89b4ec3a24629
Dion Pettaway <dionp@fullsail.edu>
2018-10-02 14:17:44 -0400

    I have the menu tray icon showing up, and it has a tooltip yatta

--- public/electron.js
+++ public/electron.js
@@ -3,13 +3,20 @@ const path = require('path');
 const {app, Tray, ipcMain, BrowserWindow} = require('electron');
 
 let mainWindow;
+let menuTrayIcon;
 
 const NOTES = []
 const WIDTH = 575;
 const HEIGHT = 650;
 const MAX_CHARACTERS = 45;
+const IMAGE = 'icon@2x.png'
+const IMAGE_PATH = path.join(__dirname, '../assets/images')
+
+const ICON = path.join(IMAGE_PATH, IMAGE)
 
 function createWindow() {
+  
+  // menuTrayIcon = new Tray(ICON)
   
   mainWindow = new BrowserWindow({
     width: WIDTH,
@@ -28,6 +35,9 @@ function createWindow() {
   } else {
     mainWindow.loadURL('http://localhost:3000')
   }
+
+  menuTrayIcon = new Tray(ICON)
+  menuTrayIcon.setToolTip('Mini-Notes');
 }
 
 app.on('ready', createWindow);
